9432bb36fd5529f0d951a69e2173906a4b5fc9b1
BintangRamadhan09 <57470221+BintangRamadhan09@users.noreply.github.com>
2019-11-25 00:12:29 +0700

    Add files via upload

--- credit.py
+++ credit.py
@@ -1,8 +1,36 @@
 import json
 
-with open('cc.json','r') as x :
-    out = json.load(x)
+with open('cc.json','r') as CC:
+    out = json.load(CC)
+def inValid(x):
+    with open('ccInvalid.json', 'w') as inV:
+        json.dump(x, inV)
 
-# with open('cc.json','r') as x :
-#     out = json.dump(x)
-    
+I =[]
+V=[]
+for i in out:
+    j = i['noCreditCard']
+    if j[0] not in '456':
+        zI.append(i)
+    elif j.isdigit() == False:
+        zI.append(i)
+    elif j[5] == ' ':
+        zI.append(i)
+    elif j[5]=='-':
+        zV.append(i)
+    elif j[0:len()]
+    elif len(j) > 16:
+        I.append(i)
+    else:
+        V.append(i)
+
+with open('ccValid.json', 'a') as ccV:
+    json.dump(V, ccV)
+
+with open('ccInvalid.json', 'a') as inV:
+    json.dump(I, inV)
+
+for i in out:
+    j = i['noCreditCard']
+    if j[0:len(j):4).isdigit] == False:
+        print(i)
